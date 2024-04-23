5f5edc7a6135125bea4576dc0297424e0284fffc
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-23 10:30:16 +0000

    m4@1.4: add 1.4.19 bottle.

--- Formula/m4@1.4.rb
+++ Formula/m4@1.4.rb
@@ -6,6 +6,12 @@ class M4AT14 < Formula
   sha256 "3be4a26d825ffdfda52a56fc43246456989a3630093cced3fbddf4771ee58a70"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/zhongruoyu/zhongruoyu-portable-ruby-aarch64-linux"
+    sha256 x86_64_linux:  "b585e0a0ba11910d7dd30550c16358353cef2e2510e9ae35f5a69d5e956a0b73"
+    sha256 aarch64_linux: "7bf5e38daaf3558ef23c6d5596c1d126be4e0c51149a3489a06f594a94917750"
+  end
+
   keg_only :versioned_formula
 
   def install
