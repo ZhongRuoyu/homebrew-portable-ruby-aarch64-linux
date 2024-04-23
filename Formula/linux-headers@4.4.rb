b44f35a80e4e648983659bf618977bb9bdb87f1b
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-23 10:54:21 +0000

    linux-headers@4.4: add 4.4.302 bottle.

--- Formula/linux-headers@4.4.rb
+++ Formula/linux-headers@4.4.rb
@@ -5,6 +5,12 @@ class LinuxHeadersAT44 < Formula
   sha256 "66271f9d9fce8596622e8154ca0ea160e46b78a5a6c967a15b55855f744d1b0b"
   license "GPL-2.0-only"
 
+  bottle do
+    root_url "https://ghcr.io/v2/zhongruoyu/zhongruoyu-portable-ruby-aarch64-linux"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "312e64f9cadbac874f0f69ae58b74ced82765ffe54b72aad66cf4baaad33154e"
+    sha256 cellar: :any_skip_relocation, aarch64_linux: "0ed7c3d1ef00c3c4f9ad62f105a156a2562fff5ea025175a0a7c756be38e485e"
+  end
+
   keg_only :versioned_formula
 
   depends_on :linux
